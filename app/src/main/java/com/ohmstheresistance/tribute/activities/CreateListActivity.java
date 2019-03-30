12bbfcb9a4b95644025b93a9ed8e7565fce96e85
Ohms TheResistance <ohmstheresistance@Ohmss-MacBook-Pro.local>
2019-03-30 09:51:51 -0400

    alert added to prompt user before deleting database completely

--- app/src/main/java/com/ohmstheresistance/tribute/activities/CreateListActivity.java
+++ app/src/main/java/com/ohmstheresistance/tribute/activities/CreateListActivity.java
@@ -1,10 +1,11 @@
 package com.ohmstheresistance.tribute.activities;
 
 
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -109,6 +110,7 @@ public class CreateListActivity extends AppCompatActivity {
         personAdapter.setPersons(people);
         personAdapter.notifyDataSetChanged();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -118,7 +120,7 @@ public class CreateListActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.database_delete_all:
 
                 deleteDatabase();
@@ -129,43 +131,65 @@ public class CreateListActivity extends AppCompatActivity {
         return true;
     }
 
-    private void deleteDatabase(){
+    private void deleteDatabase() {
 
-        Toast.makeText(CreateListActivity.this, "Database Cleared" , Toast.LENGTH_LONG).show();
-        Disposable disposable = Observable.create(new ObservableOnSubscribe<Object>() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(CreateListActivity.this);
+        dialog.setCancelable(false);
+        dialog.setTitle("This option deletes the entire database!");
+        dialog.setMessage("Are you sure you want to complete this action?");
+        dialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
             @Override
-            public void subscribe(ObservableEmitter<Object> emitter) throws Exception {
+            public void onClick(DialogInterface dialog, int id) {
 
-                personRepository.deleteAllPersons();
-                emitter.onComplete();
+                Toast.makeText(CreateListActivity.this, "Database Cleared", Toast.LENGTH_LONG).show();
+                Disposable disposable = Observable.create(new ObservableOnSubscribe<Object>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Object> emitter) throws Exception {
+
+                        personRepository.deleteAllPersons();
+                        emitter.onComplete();
+
+                    }
+                })
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(new Consumer<Object>() {
+
+                            @Override
+                            public void accept(Object o) throws Exception {
+
+                            }
+                        }, new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Exception {
+                                Toast.makeText(CreateListActivity.this, "Database Cleared" + throwable.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }, new Action() {
+                            @Override
+                            public void run() throws Exception {
+
+                            }
+                        });
+                // compositeDisposable.add(disposable);
 
             }
+
         })
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Consumer<Object>() {
 
+                .setNegativeButton("NO ", new DialogInterface.OnClickListener() {
                     @Override
-                    public void accept(Object o) throws Exception {
-
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        Toast.makeText(CreateListActivity.this, "Database Cleared" + throwable.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                }, new Action() {
-                    @Override
-                    public void run() throws Exception {
-
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Action for "Cancel".
                     }
                 });
-        // compositeDisposable.add(disposable);
+
+        final AlertDialog alert = dialog.create();
+        alert.show();
 
     }
 
 
-    }
+}
 
 
 
