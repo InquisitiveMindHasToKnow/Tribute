df9e3d836a97284d80c963070557d8d729cbf00f
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-12-29 19:43:21 -0500

    Splash screen image, background, and delay timer changed.

--- app/src/main/java/com/ohmstheresistance/tribute/intro/SplashWelcome.java
+++ app/src/main/java/com/ohmstheresistance/tribute/intro/SplashWelcome.java
@@ -4,16 +4,17 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.ohmstheresistance.tribute.R;
 import com.ohmstheresistance.tribute.activities.LoginActivity;
 
 public class SplashWelcome extends AppCompatActivity {
 
     ImageView splashImage;
-    private static int SPLASH_SCREEN_TIMER = 5500;
+    private static int SPLASH_SCREEN_TIMER = 2000;
     private Intent toLoginScreenIntent;
 
     @Override
@@ -23,9 +24,13 @@ public class SplashWelcome extends AppCompatActivity {
 
         splashImage = findViewById(R.id.splash_screen_image);
 
-        Glide.with(SplashWelcome.this)
-                .load(R.drawable.newestsplashimage)
-                .into(splashImage);
+
+        Animation blinkingAnimation = new AlphaAnimation(0.0f, 1.0f);
+        blinkingAnimation.setDuration(100);
+        blinkingAnimation.setStartOffset(300);
+        blinkingAnimation.setRepeatMode(Animation.REVERSE);
+        blinkingAnimation.setRepeatCount(Animation.INFINITE);
+        splashImage.startAnimation(blinkingAnimation);
 
 
         new Handler().postDelayed(new Runnable() {
